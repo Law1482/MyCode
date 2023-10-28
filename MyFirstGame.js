5d5c3d9f3e77a1af7b5c7ce175fc1db0f51316e1
Law1482 <148838660+Law1482@users.noreply.github.com>
2023-10-28 08:40:49 +0800

    MY First Game
    Using HTML, JavaScript AND Css.

---
+++ MyFirstGame.js
@@ -0,0 +1,85 @@
+var Player = document.getElementById("player");
+var print = document.getElementById("space");
+var print1 = document.getElementById("space1");
+var map = document.getElementById("map");
+var mapinfo = window.getComputedStyle(map);
+var mapwidth = mapinfo.getPropertyValue("width");
+ 
+var mapWidth = map.offsetWidth;
+var mapHeight = map.offsetHeight;
+var playX = 0;
+var playY = 0;
+var step = 20;
+var offWidth = Player.offsetWidth;
+var offHeight = Player.offsetHeight;
+			
+			function move(X, Y) {
+			    Player.style.left = X + "px";
+			    Player.style.top = Y + "px";
+			    Player.classList.add("animate");
+			    print.innerHTML = X;
+			    print1.innerHTML = Y;
+			}
+			
+  function move1(X, Y) {
+      var resultX = mapWidth - X - 28;
+      var resultY = mapHeight - Y - 28;
+      if(resultX <= step) {
+          playX+=resultX
+          move(playX,playY);
+      }
+      if(resultY <= step) {
+          playY+=resultY;
+          move(playX,playY);
+      }
+  }
+
+					function move2(X, Y) {
+					    
+					    if(X <= step) {
+					        playX-=X;
+					        move(playX, playY);
+					    } else if(Y <= step) {
+					        playY-=Y;
+					        move(playX, playY);
+					    }
+					}
+
+			
+			function right() {
+			    if(playX + step <= mapWidth - offWidth) {
+			        playX += step;
+			        move(playX, playY);
+						
+						} else {
+						    move1(playX, playY);
+						}
+			}
+			
+			function left() {
+			    if(playX - step >= 0) {
+			        playX -= step;
+			        move(playX, playY);
+						} else {
+						    move2(playX, playY);
+						}
+			}
+			
+			function up() {
+			    if(playY - step >= 0) {
+			        playY -= step;
+			        move(playX, playY);
+				} else {
+				    move2(playY, playY);
+				}
+			}
+			
+			function down() {
+						if(playY + step <= mapHeight - offHeight) {
+									playY += step;
+									move(playX, playY);
+						} else {
+						    move1(playX,playY);
+						}
+			}
+
